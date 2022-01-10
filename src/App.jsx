2ed7ef69e2cc6a95c2d898e4e4a43bb19de3be6c
bxxmi <bmi6638@gmail.com>
2022-01-10 12:34:31 +0900

    feat: create data

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import "./App.css";
-import Hello from "./component/Hello";
+import Header from "./component/Header";
 
 function App() {
   return (
     <div className="App">
-      <Hello />
+      <Header />
     </div>
   );
 }
--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -4,13 +4,13 @@ const Header = () => {
   return (
     <div className="header">
       <h1>
-        <a href="javascript:void(0)">토익 영단어(고급)</a>
+        <a href="#">토익 영단어(고급)</a>
       </h1>
       <div className="menu">
-        <a href="javascript:void(0)" className="link">
+        <a href="#" className="link">
           단어 추가
         </a>
-        <a href="javascript:void(0)" className="link">
+        <a href="#" className="link">
           Day 추가
         </a>
       </div>
